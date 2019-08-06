299666dbb744d586bd01b875967bda75cb0bb1b1
Nestor Freites <ferrosulfuro@gmail.com>
2019-08-06 17:04:00 +0200

    restart, cleaning

--- src/main/java/exceptions/NegativeAccountException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class NegativeAccountException extends Exception{
-	
-	public NegativeAccountException() {
-		super("The transaction would leave the account balance below 0.");
-	}
-	
-}
--- src/main/java/exceptions/ValidationException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-import java.util.List;
-
-public class ValidationException extends Exception{
-	
-	public ValidationException(String fields) {
-		super("The field/s: " + fields + " is/are null or not a valid input.");
-	}
-}
